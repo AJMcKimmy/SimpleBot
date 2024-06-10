852c8623b071870feb02dd276c40d54ad67fdf08
AJ McKimmy <32497161+PadenFain@users.noreply.github.com>
2024-06-09 23:40:02 -0700

    created UIElements class to debloat MainPage script

--- MainPage.cs
+++ MainPage.cs
@@ -10,38 +10,40 @@ using DSharpPlus.Entities;
 public partial class MainPage : Node2D
 {
 
-	public Button startButton;
-	public RichTextLabel consoleWindow;
-	public Godot.Timer buttonTimer;
-	public ItemList guildList;
-	public RichTextLabel chatWindow;
-	public LineEdit prefixField;
-	public TextEdit responseField;
-	public Button submitButton;
-	public Button addCommandButton;
-	public Button changeTokenButton;
-	public LineEdit tokenField;
-	CancellationTokenSource tokensource2 = new CancellationTokenSource();
-	public CancellationToken ct;
-	public bool initialized = false;
-	public bool buttonOnCooldown = false;
-	public bool fieldActive = false;
-	public List<string> commandList = new List<string>();
+	private Button startButton;
+	private RichTextLabel consoleWindow;
+	private Godot.Timer buttonTimer;
+	private ItemList guildList;
+	private RichTextLabel chatWindow;
+	private LineEdit prefixField;
+	private TextEdit responseField;
+	private Button submitButton;
+	private Button addCommandButton;
+	private Button changeTokenButton;
+	private LineEdit tokenField;
+	private CancellationTokenSource tokensource2 = new CancellationTokenSource();
+	private CancellationToken ct;
+	private bool initialized = false;
+	private bool buttonOnCooldown = false;
+	private bool fieldActive = false;
+	private List<string> commandList = new List<string>();
 
-	//ready runs on launch and pulls all UI elements into our main script here(not a true main script since godot compiles down to C++ at runtime)
+	UIElementsList uIElements = new UIElementsList();
+
+	//ready runs on launch and pulls all UI elements into our main script here
 	public override void _Ready()
 	{
-		startButton = GetNode<Button>("CanvasLayer/Initialize");
-		consoleWindow = GetNode<RichTextLabel>("CanvasLayer/ConsoleBorder/Console");
-		buttonTimer = GetNode<Godot.Timer>("ButtonTimer");
-		guildList = GetNode<ItemList>("CanvasLayer/GuildsList");
-		chatWindow = GetNode<RichTextLabel>("CanvasLayer/ChatLog");
-		submitButton = GetNode<Button>("CanvasLayer/SubmitCommandButton");
-		prefixField = GetNode<LineEdit>("CanvasLayer/PrefixBox");
-		responseField = GetNode<TextEdit>("CanvasLayer/ResponseBox");
-		addCommandButton = GetNode<Button>("CanvasLayer/AddCommandButton");
-		changeTokenButton = GetNode<Button>("CanvasLayer/ChangeTokenButton");
-		tokenField = GetNode<LineEdit>("CanvasLayer/TokenField");
+		uIElements.startButton = GetNode<Button>("CanvasLayer/Initialize");
+		uIElements.consoleWindow = GetNode<RichTextLabel>("CanvasLayer/ConsoleBorder/Console");
+		uIElements.buttonTimer = GetNode<Godot.Timer>("ButtonTimer");
+		uIElements.guildList = GetNode<ItemList>("CanvasLayer/GuildsList");
+		uIElements.chatWindow = GetNode<RichTextLabel>("CanvasLayer/ChatLog");
+		uIElements.submitButton = GetNode<Button>("CanvasLayer/SubmitCommandButton");
+		uIElements.prefixField = GetNode<LineEdit>("CanvasLayer/PrefixBox");
+		uIElements.responseField = GetNode<TextEdit>("CanvasLayer/ResponseBox");
+		uIElements.addCommandButton = GetNode<Button>("CanvasLayer/AddCommandButton");
+		uIElements.changeTokenButton = GetNode<Button>("CanvasLayer/ChangeTokenButton");
+		uIElements.tokenField = GetNode<LineEdit>("CanvasLayer/TokenField");
 		ct = tokensource2.Token;
 		if (File.Exists("commandlist.txt"))
 		{
@@ -59,23 +61,23 @@ public partial class MainPage : Node2D
 		//delay enforced to allow for connection time using godot's built in timer node
 		if (!buttonOnCooldown)
 		{
-			buttonTimer.Start();
+			uIElements.buttonTimer.Start();
 			buttonOnCooldown = true;
 			if (!initialized)
 			{
-				startButton.Text = "Disconnect";
+				uIElements.startButton.Text = "Disconnect";
 				initialized = true;
-				consoleWindow.Text += ">>> Attempting to start bot\n";	
+				uIElements.consoleWindow.Text += ">>> Attempting to start bot\n";	
 				tokensource2 = new CancellationTokenSource();
 				ct = tokensource2.Token;		
-				await BotStart(startButton, consoleWindow, ct, guildList, chatWindow, commandList);			
+				await BotStart(uIElements.startButton, uIElements.consoleWindow, ct, uIElements.guildList, uIElements.chatWindow, commandList);			
 			}	
 			else if (initialized)
 			{
 				initialized = false;
-				startButton.Text = "Connect";
-				consoleWindow.Text += ">>> Bot Stopped!\n";
-				guildList.Clear();
+				uIElements.startButton.Text = "Connect";
+				uIElements.consoleWindow.Text += ">>> Bot Stopped!\n";
+				uIElements.guildList.Clear();
 				tokensource2.Cancel();
 				tokensource2 = new CancellationTokenSource();
 				ct = tokensource2.Token;
@@ -178,40 +180,40 @@ public partial class MainPage : Node2D
 		commandList.Add(responseField.Text);
 		File.AppendAllText("commandlist.txt", prefixField.Text + System.Environment.NewLine);
 		File.AppendAllText("commandlist.txt", responseField.Text + System.Environment.NewLine);
-		responseField.Clear();
-		prefixField.Clear();
-		consoleWindow.Text += ">>> Added command successfully!\n";
-		prefixField.Visible = false;
-		responseField.Visible = false;
-		submitButton.Visible = false;
-		addCommandButton.Visible = true;
+		uIElements.responseField.Clear();
+		uIElements.prefixField.Clear();
+		uIElements.consoleWindow.Text += ">>> Added command successfully!\n";
+		uIElements.prefixField.Visible = false;
+		uIElements.responseField.Visible = false;
+		uIElements.submitButton.Visible = false;
+		uIElements.addCommandButton.Visible = true;
 
 	}
 	private void _on_add_command_button_pressed()
 	{
-		submitButton.Visible = true;
-		prefixField.Visible = true;
-		responseField.Visible = true;
-		addCommandButton.Visible = false;
+		uIElements.submitButton.Visible = true;
+		uIElements.prefixField.Visible = true;
+		uIElements.responseField.Visible = true;
+		uIElements.addCommandButton.Visible = false;
 	}
 
 	private async void _on_change_token_button_pressed()
 	{
 		if (!fieldActive)
 		{
-			tokenField.Visible = true;
-			changeTokenButton.Text = "Submit";
+			uIElements.tokenField.Visible = true;
+			uIElements.changeTokenButton.Text = "Submit";
 			fieldActive = true;
 		}
 		else if(fieldActive)
 		{
-			String token = tokenField.Text;
+			String token = uIElements.tokenField.Text;
 			JSONReader configJsonFile = new JSONReader();
 			fieldActive = false;
 			await configJsonFile.WriteJSON(token);
-			tokenField.Visible = false;
-			changeTokenButton.Text = "Change Token";
-			consoleWindow.Text += ">>> Token Updated";
+			uIElements.tokenField.Visible = false;
+			uIElements.changeTokenButton.Text = "Change Token";
+			uIElements.consoleWindow.Text += ">>> Token Updated\n";
 		}
 		
 	}
---
+++ UIElements.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+internal class UIElementsList()
+{
+    public Button startButton;
+	public RichTextLabel consoleWindow;
+	public Godot.Timer buttonTimer;
+	public ItemList guildList;
+	public RichTextLabel chatWindow;
+	public LineEdit prefixField;
+	public TextEdit responseField;
+	public Button submitButton;
+	public Button addCommandButton;
+	public Button changeTokenButton;
+	public LineEdit tokenField;
+}
